f9c7fb5d0c6ca21579b9410de1edee120048975f
root <root@DESKTOP-SKBK36U>
2023-10-21 00:28:22 -0300

    fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,8 +3,8 @@ import { notFound } from 'next/navigation';
 import './globals.css'
 import { Young_Serif, Kanit } from 'next/font/google'
 
-const young_Serif = Young_Serif({ subsets: ['latin-ext'], weight: ['400'] })
-const kanit = Kanit({ subsets: ['latin-ext'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
+const young_Serif = Young_Serif({ subsets: ['latin-ext'], weight: ['400'], variable: '--young-serif-font-family'})
+const kanit = Kanit({ subsets: ['latin-ext'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], variable: '--kanit-font-family' })
 export function generateStaticParams() {
   return [{ locale: 'pt-br' }, { locale: 'en' }];
 }
@@ -25,18 +25,7 @@ export default async function LocaleLayout({
 
   return (
   <html lang={locale}>
-      <style>
-        {`
-          :root {
-            --young-serif-font-family: ${young_Serif.style.fontFamily};
-            --kanit-font-family: ${kanit.style.fontFamily};
-          }
-          html {
-            font-family: var(--kanit-font-family);
-          }
-        `}
-      </style>
-      <body className={`${young_Serif} ${kanit}`} >
+      <body className={`${young_Serif.variable} ${kanit.variable}`} >
         <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
