9c8999cc701705dfc09e956bc95312214f7e7c39
root <root@DESKTOP-SKBK36U>
2023-10-20 22:53:42 -0300

    fix

--- app/[locale]/components/mainBanner.tsx
+++ app/[locale]/components/mainBanner.tsx
@@ -1,22 +1,39 @@
 'use client';
 import Link from 'next/link';
 import styles from './mainBanner.module.css'
+import { useTranslations } from 'next-intl';
 
 export default function MainBanner() {
+    const t = useTranslations('Index');
 
     return (
       <>
         <main className={styles.main}>
           <div className={styles.mainContainer}>
-            <h1>
-              A oportunidade 
-              de ter um pedaço
-              do paraíso chegou
-              até você. 
-            </h1>
+          <h1 dangerouslySetInnerHTML={{ __html: t('MainBanner').replace(/(paradise|paraíso|pedaço do|piece of|pedazo del)/g, (match) => {
+            switch (match) {
+              case 'paradise':
+              case 'paraíso':
+                return '<strong>' + match + '</strong>';
+              case 'pedaço do':
+              case 'piece of':
+              case 'pedazo del':
+                return '<span>' + match + '</span>';
+              default:
+                return match;
+            }
+          }) }}>
+          </h1>
+
+            <br />
             <Link href="/">
-              <button>
-                <p>RESERVE SUA UNIDADE</p>
+              <button className={styles.MainButton}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: t('Navbar.Reserve').toLocaleUpperCase().replace(/(unidade|UNIDADE|UNIT|unit|UNIDAD|unidad)/g, '<span>$1</span>')
+                }}
+              ></p>
+
               </button>
             </Link>
           </div>
