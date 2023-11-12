031e827a0f0e2f05c486087fb7404dfdd1607a3c
root <root@DESKTOP-SKBK36U>
2023-11-12 13:32:07 -0300

    testing

--- app/common/carrousel/index.tsx
+++ app/common/carrousel/index.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import {useTranslations} from 'next-intl';
 import styles from './styles.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import { Autoplay, Virtual } from 'swiper/modules';
+import SocialMidiaIcon from '../socialMidiaIcons';
 export default function Carrousel() {
   const t = useTranslations('Index');
   return (
@@ -31,9 +36,39 @@ export default function Carrousel() {
             }
           }) }}>
           </h3>
-          <div>
-            carrousel
+          <div className={styles.swiperHere}>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                centeredSlides={false}
+                loop={false}
+                grabCursor={true}
+                modules={[Autoplay, Virtual]}
+                autoplay={{
+                  delay: 2000,
+                  disableOnInteraction: true,
+                }}
+                className={styles.swiperMain}
+              >
+                  <SwiperSlide key={1} className={styles.Slide}>
+                    carrousel
+                  
+                  </SwiperSlide>
+                  <SwiperSlide key={2} className={styles.Slide}>
+                    carrousel2
+                  
+                  </SwiperSlide>
+                  <SwiperSlide key={3} className={styles.Slide}>
+                    carrousel3
+                  
+                  </SwiperSlide>
+                  <SwiperSlide key={4} className={styles.Slide}>
+                    carrousel4
+                  
+                  </SwiperSlide>
+            </Swiper>
           </div>
+          <br />
           <button>
             <p><span>{t('assessment.lastButton').toUpperCase()}{' '}</span> {t('assessment.lastButtonMain').toUpperCase()}</p>
           </button>
