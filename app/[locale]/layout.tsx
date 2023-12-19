6e2730f59952d4fc41d89e51389cfba8f1a4d0a0
root <root@DESKTOP-SKBK36U>
2023-12-18 22:36:17 -0300

    fix items pre production

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Young_Serif, Kanit } from 'next/font/google'
 import GraphQLProvider from '../lib/provider';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import {createTranslator} from 'next-intl';
 import Script from 'next/script';
 const young_Serif = Young_Serif({ subsets: ['latin-ext'], weight: ['400'], variable: '--young-serif-font-family'})
@@ -134,8 +135,8 @@ export default async function LocaleLayout({
         `}}>
       </Script>
       <noscript>
-        <img height="1" width="1" style={{display:"none"}} 
-            src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"/>
+        <Image height="1" width="1" style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1" alt={''}/>
       </noscript>
         <GraphQLProvider>
           <NextIntlClientProvider locale={locale} messages={messages}>
